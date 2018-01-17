455125bbe21814ddc84533ab9a7bc802db0d02f3
Robert Hilton <robert.a.hilton.jr@gmail.com>
2018-01-17 13:22:39 -0500

    Remove unneeded projects

--- InstanceTest/Debug/src/InstanceTest.d
+++
@@ -1,3 +0,0 @@
-src/InstanceTest.o: ../src/InstanceTest.cpp ../src/TestClass.h
-
-../src/TestClass.h:
--- InstanceTest/Debug/src/TestClass.d
+++
@@ -1,3 +0,0 @@
-src/TestClass.o: ../src/TestClass.cpp ../src/TestClass.h
-
-../src/TestClass.h:
