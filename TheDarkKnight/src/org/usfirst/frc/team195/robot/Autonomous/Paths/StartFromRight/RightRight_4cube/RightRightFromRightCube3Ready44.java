76a0129a9e4bf5697f9cfd6d8ac4f6edd36c2e86
Chris Bonomi <bonomi.c11@gmail.com>
2018-03-27 22:08:41 -0400

    Auto updates

--- TheDarkKnight/src/org/usfirst/frc/team195/robot/Autonomous/Paths/StartFromRight/RightRight_4cube/RightRightFromRightCube3Ready44.java
+++ TheDarkKnight/src/org/usfirst/frc/team195/robot/Autonomous/Paths/StartFromRight/RightRight_4cube/RightRightFromRightCube3Ready44.java
@@ -6,25 +6,26 @@ import org.usfirst.frc.team195.robot.Utilities.TrajectoryFollowingMotion.PathBui
 
 import java.util.ArrayList;
 
-public class RightRightFromRightCube3GOBACK implements PathContainer {
+public class RightRightFromRightCube3Ready44 implements PathContainer {
 
 	@Override
 	public Path buildPath() {
 		ArrayList<Waypoint> sWaypoints = new ArrayList<Waypoint>();
-		sWaypoints.add(new Waypoint(227,108,0,0));
-		sWaypoints.add(new Waypoint(260,80,15,50,"PreparePlaceCube"));
-		sWaypoints.add(PathAdapter.getAdaptedRightScaleWaypoint(new Waypoint(286,86,0,30)));
+		sWaypoints.add(PathAdapter.getAdaptedRightScaleWaypoint(new Waypoint(286,86,0,0)));
+		sWaypoints.add(new Waypoint(262,81,15,50));
+		sWaypoints.add(new Waypoint(239,100,10,50));
+		sWaypoints.add(new Waypoint(232,118,0,50));
 
 		return PathBuilder.buildPathFromWaypoints(sWaypoints);
 	}
 
 	@Override
 	public RigidTransform2d getStartPose() {
-		return new RigidTransform2d(new Translation2d(227, 108), Rotation2d.fromDegrees(180));
+		return new RigidTransform2d(new Translation2d(286, 86), Rotation2d.fromDegrees(180));
 	}
 
 	@Override
 	public boolean isReversed() {
-		return true;
+		return false;
 	}
 }
