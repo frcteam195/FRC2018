c6b74052f29f7e010cc7b852521ddf2b206a2f45
Robert Hilton <robert.a.hilton.jr@gmail.com>
2018-07-13 00:14:55 -0400

    Add IRI field profile and set to current

---
+++ TheDarkKnight_Gradle/src/main/java/org/usfirst/frc/team195/robot/Autonomous/Paths/Fields/IRIField.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc.team195.robot.Autonomous.Paths.Fields;
+
+public class IRIField extends FieldProfile {
+	public IRIField() {
+		mRedFrontLeftSwitchToBackWall = 139.5;
+		mRedBackLeftSwitchToBackWall = 196;
+		mRedLeftSwitchToSideWall = 84.25;
+		mRedLeftScaleToBackWall = 298.75;
+		mRedLeftScaleToSideWall = 70.5;
+
+		mRedFrontRightSwitchToBackWall = 140;
+		mRedBackRightSwitchToBackWall = 196.5;
+		mRedRightSwitchToSideWall = 82.5;
+		mRedRightScaleToBackWall = 301;
+		mRedRightScaleToSideWall = 69.5;
+
+		mBlueFrontLeftSwitchToBackWall = 140;
+		mBlueBackLeftSwitchToBackWall = 196.5;
+		mBlueLeftSwitchToSideWall = 82.5;
+		mBlueLeftScaleToBackWall = 298.75;
+		mBlueLeftScaleToSideWall = 69.5;
+
+		mBlueFrontRightSwitchToBackWall = 139.5;
+		mBlueBackRightSwitchToBackWall = 196.25;
+		mBlueRightSwitchToSideWall = 84.25;
+		mBlueRightScaleToBackWall = 300.75;
+		mBlueRightScaleToSideWall = 70.5;
+
+		mSideWalltoSideWall = 321;
+	}
+}
